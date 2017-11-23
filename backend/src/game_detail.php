5ca99cd0a2f570633ead003c3b70cdc9cebab760
Ubuntu <ubuntu@ip-172-31-22-71.us-east-2.compute.internal>
2017-11-23 01:06:06 +0000

    Added routes to post new game and update game details.

--- backend/src/game_detail.php
+++ backend/src/game_detail.php
@@ -12,3 +12,30 @@ $app->get('/game-detail/{id}', function(Request $request, Response $response, ar
   $game = $pdo->fetchAll();
 	return $this->response->withJson($game);
 });
+
+// Submit a new game
+$app->post('/new-game', function(Request $request, Response $response){
+  $input = $request->getParsedBody();
+  $pdo = $this->db->prepare("INSERT INTO games (picture_url, location, description, time_created, UserID, date_of_game) VALUES (:picture_url, :location, :description, CURRENT_TIMESTAMP, :UserID, :date_of_game)");
+  $pdo->bindParam("picture_url", $input["picture_url"]);
+  $pdo->bindParam("location", $input["location"]);
+  $pdo->bindParam("description", $input["description"]);
+  $pdo->bindParam("UserID", $input["UserID"]);
+  $pdo->bindParam("date_of_game", $input["date_of_game"]);
+  $pdo->execute();
+        return $this->response->withJson($input);
+});
+
+// Update game details
+$app->put('/game-detail/{GameID}', function(Request $request, Response $response, array $args) {
+  $input = $request->getParsedBody();
+  $pdo = $this->db->prepare("UPDATE games SET picture_url=:picture_url, location=:location, description=:description, date_of_game=:date_of_game WHERE GameID=:GameID");
+  $pdo->bindParam("GameID", $args["GameID"]);
+  $pdo->bindParam("picture_url", $input["picture_url"]);
+  $pdo->bindParam("location", $input["location"]);
+  $pdo->bindParam("description", $input["description"]);
+  $pdo->bindParam("date_of_game", $input["date_of_game"]);
+  $pdo->execute();
+  $input['GameID'] = $args['GameID'];
+        return $this->response->withJson($input);
+});
