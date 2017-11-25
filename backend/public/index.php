45312a645ac404512beb524833609db930fd2aba
Ubuntu <ubuntu@ip-172-31-22-71.us-east-2.compute.internal>
2017-11-25 07:49:30 +0000

    pushing before editing

--- backend/public/index.php
+++ backend/public/index.php
@@ -29,14 +29,14 @@ require __DIR__ . '/../src/middleware.php';
 require __DIR__ . '/../src/login.php';
 require __DIR__ . '/../src/registration.php';
 require __DIR__ . '/../src/games.php';
-require __DIR__ . '/../src/profileGet.php';
+require __DIR__ . '/../src/get_profile.php';
 require __DIR__ . '/../src/game_detail.php';
 require __DIR__ . '/../src/delete_game.php';
 require __DIR__ . '/../src/post_location.php';
 require __DIR__ . '/../src/get_location.php';
 require __DIR__ . '/../src/rsvp.php';
-require __DIR__ . '/../src/users.php';
-require __DIR__ . '/../src/app_location.php';
+require __DIR__ . '/../src/approve_admin.php';
+require __DIR__ . '/../src/approve_location.php';
 require __DIR__ . '/../src/pofile-update.php';
 
 // Add dynamic ones below this line --------------------------------------------
--- backend/vendor/composer/autoload_real.php
+++ backend/vendor/composer/autoload_real.php
@@ -23,35 +23,24 @@ class ComposerAutoloaderInit208d810c53d99749b14d888c46b3112e
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit208d810c53d99749b14d888c46b3112e', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+        $map = require __DIR__ . '/autoload_namespaces.php';
+        foreach ($map as $namespace => $path) {
+            $loader->set($namespace, $path);
+        }
 
-            call_user_func(\Composer\Autoload\ComposerStaticInit208d810c53d99749b14d888c46b3112e::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
+        $map = require __DIR__ . '/autoload_psr4.php';
+        foreach ($map as $namespace => $path) {
+            $loader->setPsr4($namespace, $path);
+        }
 
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
+        $classMap = require __DIR__ . '/autoload_classmap.php';
+        if ($classMap) {
+            $loader->addClassMap($classMap);
         }
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit208d810c53d99749b14d888c46b3112e::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
+        $includeFiles = require __DIR__ . '/autoload_files.php';
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequire208d810c53d99749b14d888c46b3112e($fileIdentifier, $file);
         }
