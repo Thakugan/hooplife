5c4a720aba4c565e69c8d80c20d5b43d878802a9
Chris Ragsdale <cragsdale@smu.edu>
2017-11-30 06:27:10 -0600

    File upload (almost works, hopefully jesus will show me the way at some point today and help me fix it), reset password on user page

--- frontend/src/app/components/user-private/user-private.component.ts
+++ frontend/src/app/components/user-private/user-private.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../_models/user';
 import { Comment } from '../../_models/comment';
 
+import { MatSnackBar } from '@angular/material';
+
 @Component({
   	selector: 'app-user-private',
   	templateUrl: './user-private.component.html',
@@ -13,16 +15,59 @@ export class UserPrivateComponent {
 
 	displayComments: boolean = false;
 	@Input() user: User;
-  @Input() comments: Comment[];
+  	@Input() comments: Comment[];
 
 	newUsername: string;
 	newEmail: string;
+	newPassword: string;
+	passwordVerify: string;
 
-	constructor(){ }
+	snackBarMessage: string = "";
 
-	updateUser(){
-		this.user.username = this.newUsername;
-		this.user.email = this.newEmail;
+	usernameChanged: boolean;
+	emailChanged: boolean;
+	passwordsMatch: boolean;
+
+	constructor( private snackBar: MatSnackBar ){ }
+
+	private updateUser(){
+
+		if(this.newUsername){
+			//attempt to change username. If unsuccessful, display alert
+			this.user.username = this.newUsername;
+			this.snackBarMessage = this.snackBarMessage + 'Username update successful\n';
+		}
+
+		if(this.newEmail){
+			this.user.email = this.newEmail;
+			this.snackBarMessage = this.snackBarMessage + 'Email update successful\n';
+		}
+
+		if(this.newPassword && this.passwordVerify){
+			if(this.newPassword === this.passwordVerify){
+				//set new password
+				this.snackBarMessage = this.snackBarMessage + 'Password update successful\n';
+			}
+
+			else{
+				this.snackBarMessage = this.snackBarMessage + 'Password update unsuccessful\n';
+			}
+		}
+
+		this.snackBar.open(this.snackBarMessage, '', {
+        	duration: 5000
+      	});
+
+      	this.snackBarMessage = "";
+
+		this.resetForm();
+	}
+
+	private resetForm(){
+		this.newUsername = null;
+		this.newEmail = null;
+		this.newPassword = null;
+		this.passwordVerify = null;
 	}
 
 }
